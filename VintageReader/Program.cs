3896ba4fa98898909abc57d94d5173d343ca0b05
Noel <garcia.guimeras@gmail.com>
2016-08-01 14:38:03 -0400

    Adding a TODO file

--- VintageReader/Program.cs
+++ VintageReader/Program.cs
@@ -13,6 +13,10 @@ namespace VintageReader
 	{
 
 		public const string NAME = "VintageReader v0.1";
+		public const string SUBTITLE = "An old-style reader for your new books";
+
+		public const string COPYRIGHT = "(c) 2016, Noel Garcia Guimeras <garcia.guimeras@gmail.com>";
+		public const string LICENSE = "Released under GNU General Public License";
 
 		public static void ShowFile(string filename)
 		{
@@ -21,6 +25,7 @@ namespace VintageReader
 			LibraryManager libraryManager = new LibraryManager();
 			libraryManager.Read();
 
+			Console.WriteLine("Reading epub...");
 			BookReader bookReader = new BookReader();
 			BookInfo bookInfo = bookReader.Read(filename);
 			if (bookInfo == null)
@@ -29,6 +34,7 @@ namespace VintageReader
 				return;
 			}	
 
+			Console.WriteLine("Loading library...");
 			LibraryInfo libraryInfo = libraryManager.Find(bookInfo);
 			if (libraryInfo == null)
 				libraryInfo = libraryManager.CreateNew(bookInfo);
@@ -43,6 +49,10 @@ namespace VintageReader
 		public static void Main(string[] args)
 		{
 			Console.WriteLine(NAME);
+			Console.WriteLine(SUBTITLE);
+			Console.WriteLine(LICENSE);
+			Console.WriteLine();
+
 			if (args.Length == 0)
 			{
 				Console.WriteLine("Missing argument: filename.");
